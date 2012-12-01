e472c7e3411a23ddc24a0f052b6f4082bcead2da
hsichang <hsi.chang@gmail.com>
2012-12-01 09:43:41 -0500

    style client side

---
+++ communikatr-client/app/assets/javascripts/communikatr.js
@@ -0,0 +1,43 @@
+$(function(){
+  // $('#send_message').click(sendMessage);
+  $('#send_message').click(sendMessage);
+});
+
+function checkButton () {
+ var id = $('input[name=delivery]:checked').val()
+ alert(id);
+}
+
+function sendMessage() {
+  // body...
+  var id = $('input[name=delivery]:checked').val()
+
+  var message = $('#message').val();
+  var address = $('#email').val();
+  var twitter = $('#handle').val();
+  var phone = $('#usrtel').val();
+
+  if (id === "text") {
+    $.ajax({
+    type: "POST",
+    url: "http://44h6.localtunnel.com/text",
+    data: { to: phone, message: message }
+  })
+  };
+  if (id === "twitter") {
+    $.ajax({
+    type: "POST",
+    url: "http://44h6.localtunnel.com/twitter",
+    data: { to: twitter, message: message }
+  })
+
+  };
+  if (id === "email") {
+    $.ajax({
+    type: "POST",
+    url: "http://44h6.localtunnel.com/email",
+    data: { to: address, message: message }
+  })
+  };
+  
+}
